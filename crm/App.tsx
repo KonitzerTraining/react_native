07f14e1fadd89aec825680d740f01d73cc184ce2
Marcus Konitzer <mk@konitzer-training.de>
2022-10-19 20:02:47 +0000

    Init

--- crm/App.tsx
+++ crm/App.tsx
@@ -2,20 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import {View, Text} from "react-native";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+       <View>
+         <Text>Hello</Text>
+         <StatusBar></StatusBar>
+       </View>
       </SafeAreaProvider>
     );
   }
