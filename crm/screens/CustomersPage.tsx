cec7f3ab0321e98f155e7336c87c367031d985ef
Marcus Konitzer <mk@konitzer-training.de>
2022-10-20 09:47:44 +0000

    Init

--- crm/screens/CustomersPage.tsx
+++ crm/screens/CustomersPage.tsx
@@ -1,18 +1,40 @@
 import {View, Text} from "react-native";
 import config from "../config";
 import axios from "axios";
+import {useEffect, useState} from "react";
 
 const url = config.api + 'customers/';
 
 function CustomersPage() {
 
-    axios.get(url).then((response) => {
-        console.log(response.data);
-    })
+    // let customers: any = [];
+
+    // Array Deconstructor
+    let [customers, setCustomers] = useState([1,2]);
+    console.log(customers);
+
+    useEffect(() => {
+        async function loadCustomersAsync() {
+
+        let response =  await axios.get(url);
+            setCustomers(response.data);
+        }
+
+        loadCustomersAsync();
+    }, []);
+
+
+/*    useEffect(() => {
+        axios.get(url).then((response) => {
+            console.log(response.data);
+            setCustomers(response.data);
+        })
+    }, []);*/
 
     return (
         <View>
             <Text>Hello world</Text>
+            {customers.length}
         </View>
     )
 }
