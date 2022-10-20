8e450eae66d85f0f3daababc44ca09b54acf47fe
Marcus Konitzer <mk@konitzer-training.de>
2022-10-20 09:19:02 +0000

    Init

--- crm/screens/CustomersPage.tsx
+++ crm/screens/CustomersPage.tsx
@@ -1,6 +1,15 @@
 import {View, Text} from "react-native";
+import config from "../config";
+import axios from "axios";
+
+const url = config.api + 'customers/';
 
 function CustomersPage() {
+
+    axios.get(url).then((response) => {
+        console.log(response.data);
+    })
+
     return (
         <View>
             <Text>Hello world</Text>
