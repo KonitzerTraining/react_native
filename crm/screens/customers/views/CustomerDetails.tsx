64ba106c8da8d5eb7696225b0151715abc7aea3e
Marcus Konitzer <mk@konitzer-training.de>
2022-10-20 13:31:09 +0000

    Init

--- crm/screens/customers/views/CustomerDetails.tsx
+++ crm/screens/customers/views/CustomerDetails.tsx
@@ -1,15 +1,31 @@
-import {View, Text} from "react-native";
+import {View, Text, Button} from "react-native";
+import axios from "axios";
+import config from "../../../config";
+
+const url = config.api + 'customers/';
+
 function CustomerDetails({route}: any) {
     const customer = route.params.customer;
+
+    async function deleteHandler() {
+        let response = await axios.delete(url + customer.id);
+        console.log(response);
+    }
 
     return (
         <View>
             <Text>
-              Name:  {customer.name}
+                Name: {customer.name}
             </Text>
             <Text>
-              Credit:  {customer.credit.toLocaleString('de-DE')} €
+                Credit: {customer.credit.toLocaleString('de-DE')} €
             </Text>
+
+            <Button
+                onPress={deleteHandler}
+                title="Delete"
+                color="#841584"
+            />
         </View>
     );
 }
